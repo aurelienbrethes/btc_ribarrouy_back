55ad6f3cfcce9d92b0c282eae67f5597b5aea1de
aurelienbrethes <aurelienbrethes@gmail.com>
2022-02-03 11:52:00 +0100

    clean code

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,31 +2,21 @@ const authRouteur = require('express').Router();
 const { getByEmail, verifyPassword } = require('../models/user');
 const { calculateToken } = require('../helpers/users');
 
-// étape 16
 authRouteur.get('/coucoucou', (req, res) => {
   res.status(200).send('hiboubou');
 });
 
-// étape 17
 authRouteur.post('/login', (req, res) => {
   const { email, password } = req.body;
-  // étape 19
   getByEmail(email)  
     .then(([users]) => users[0])
     .then((user) => {
       if (!user) {
         res.status(422).send('email incorrect');
       } else {
-        // étape 21
-        verifyPassword(password, user.hashedPassword).then((passwordOk) => {
-          // étape 22
-          
-          if (passwordOk) {
-            // étape 23
-            // générer un token
-            const token = calculateToken(email, user.id); // étape 24 - rajout de l'id
-
-            // l'envoyer par cookie
+        verifyPassword(password, user.hashedPassword).then((passwordOk) => {        
+          if (passwordOk) { 
+            const token = calculateToken(email, user.id); 
             res.cookie('monCookie', token);
             res.send();
           }
